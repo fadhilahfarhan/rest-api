7f04e7fa0e4be26e968098031d24e37a11a336fe
fadh21268ti <fadh21268ti@nurulfikri.ac.id>
2022-10-31 10:00:52 +0700

    upload file

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,21 +2,26 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use app\Models\Student;
 
 class StudentController extends Controller
 {
     public function index()
     {
-        $user = [
-            'nama' => 'Fadhilah Farhan',
-            'jurusan' => 'Teknik Informatika'
+        # menggunakan model students untul select datanya
+        $students = Student::all();
+
+        $data = [
+            'message' => 'Get all student',
+            'data' => $students
         ];
 
         # menggunakan response json laravel
         # otomatis set header content type json
         # otomatis mengubah data array  ke json
         # mengatur status code
-        return response()->json($user,200);
+        return response()->json($data, 200);
     }
 }
