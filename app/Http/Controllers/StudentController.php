fe0b9be039aa422e734537fcc78a0eb6b2879b28
FadhilahFarhan <fadhilahfarhan2004@gmail.com>
2022-11-08 22:32:20 +0700

    Updated

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -51,7 +51,7 @@ class StudentController extends Controller
 
     #untuk mengupdate data
     public function update(Request $request, $id)
-    {
+    { 
         #mencari data menggunakan find id
         $student = Student::find($id);
         #memvalidasi data yang diinput
